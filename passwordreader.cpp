49560fe689c98a50c1f303cb08b304380281e879
vamsisambangi <42500111+vamsisambangi@users.noreply.github.com>
2020-06-19 19:52:33 +0530

    Add files via upload
    password reader added

---
+++ passwordreader.cpp
@@ -0,0 +1,44 @@
+// C code to illustrate working of 
+// getch() to accept hidden inputs 
+
+#include <conio.h> 
+#include <dos.h> // delay() 
+#include <stdio.h> 
+#include <string.h> 
+
+int main() 
+{ 
+    
+	// Taking the password of 8 characters 
+	char pwd[9]; 
+	int i; 
+
+	 
+
+	printf("Enter Password: "); 
+	for (i = 0; i < 8; i++) { 
+
+		// Get the hidden input 
+		// using getch() method 
+		pwd[i] = getch(); 
+
+		// Print * to show that 
+		// a character is entered 
+		printf("*"); 
+	} 
+	pwd[i] = '\0'; 
+	printf("\n"); 
+
+	// Now the hidden input is stored in pwd[] 
+	// So any operation can be done on it 
+
+	// Here we are just printing 
+	printf("Entered password: "); 
+	for (i = 0; pwd[i] != '\0'; i++) 
+		printf("%c", pwd[i]); 
+
+	// Now the console will wait 
+	// for a key to be pressed 
+	return 0; 
+} 
+
